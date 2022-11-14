09341f52fab3b90aa8583c7002c475a5c5e6f69a
NicholasPaparusso <nicopaparusso@gmail.com>
2022-11-14 15:25:20 +0100

    [add] AppTitle component in App.vue

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'AppTitle',
+
+  data(){
+  return{
+    title: 'La mia prima app con Vite!'
+  }
+ }
+ }
+
+</script>
+
+<template>
+  <div>
+    <h1>
+      {{title}}
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+
+  h1{
+    text-align: center;
+    color: lightblue
+  }
+
+</style>
